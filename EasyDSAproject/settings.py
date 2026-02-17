878f4cc126bfb7e4a20fb87a22edc1a1b9723c9a
Payal Mohapatra <payalmohapatra137@gmail.com>
2026-02-17 20:03:26 +0530

    Added custom domain to ALLOWED_HOSTS

--- EasyDSAproject/settings.py
+++ EasyDSAproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['easydsapro.onrender.com','localhost', '127.0.0.1' ,'.onrender.com']
+ALLOWED_HOSTS = ['easydsapro.onrender.com', 'www.easydsa.online','easydsa.online','localhost', '127.0.0.1' ,'.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://easydsapro.onrender.com',
